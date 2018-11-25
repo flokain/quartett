93e5dd9cb24dda39b30a0b486faab38ff5a00888
florian kain <fk@ong.at>
2018-11-25 20:44:21 +0100

    exportieren möglic. lesen von google spreadsheet möglich

---
+++ src/quartettCreator.py
@@ -0,0 +1,48 @@
+from pandas import DataFrame
+
+from dataReaders import get_google_sheet, gsheet2df
+from xmlManipulator import renderAsPdf, substituteSVG
+
+
+def testGenerator():
+    values = {}
+    values['card_name'] = "Blaumeise"
+
+    tree = substituteSVG(values)
+    renderAsPdf(tree, values['card_name'])
+
+
+def testExcelReading():
+    SPREADSHEET_ID = '1CJKcriAlXe6nPxcFuNbdgMF24N5SN2lu7lQBmlBdtGc'
+    RANGE_NAME = 'daten'
+
+    gsheet = get_google_sheet(SPREADSHEET_ID, RANGE_NAME)
+
+    df = gsheet2df(gsheet)
+
+    # print('Dataframe size = ', df.shape)
+    # print(df.head())
+    # print(df.all)
+
+    print(df['card_file_name'].iloc[1])
+
+def generateQuartett():
+
+    SPREADSHEET_ID = '1CJKcriAlXe6nPxcFuNbdgMF24N5SN2lu7lQBmlBdtGc'
+    RANGE_NAME = 'daten'
+
+    gsheet = get_google_sheet(SPREADSHEET_ID, RANGE_NAME)
+    df = gsheet2df(gsheet)
+
+    dict_data = df.to_dict(orient='records')
+    for values in dict_data:
+        tree = substituteSVG(values)
+        renderAsPdf(tree, values['card_file_name'])
+
+
+
+
+# os.rmdir('out/tmp')
+testGenerator()
+testExcelReading()
+generateQuartett()
--- src/xmlManipulator.py
+++ src/xmlManipulator.py
@@ -1,82 +1,102 @@
 import os
+from subprocess import Popen, call
 from typing import Dict
 
 print(os.getcwd())
 
 import xml.etree.ElementTree as ET
 
-tree = ET.parse('./resourcen/template.svg')
-root = tree.getroot()
 
-namespaces = {'svg': 'http://www.w3.org/2000/svg',
-              'xlink': "http://www.w3.org/1999/xlink"}
+def substituteSVG(values):
+    tree = ET.parse('../resourcen/template.svg')
+    root = tree.getroot()
 
-Textelectors = {"card_name": {'xpath': ".//*[@id='%s']/svg:tspan"},
-                "card_subtitle": {'xpath': ".//*[@id='%s']/svg:tspan"},
-                "category_": {'xpath': ".//*[@id='%s']/svg:tspan"},
-                "val_": {'xpath': ".//*[@id='%s']/svg:tspan"},
-                "description_1": {'xpath': ".//*[@id='%s']/svg:flowPara"},
-                "description_2": {'xpath': ".//*[@id='%s']/svg:flowPara"},
-                "top_right_text": {'xpath': ".//*[@id='%s']/svg:tspan"},
-                "mid_right_text": {'xpath': ".//*[@id='%s']/svg:tspan"},
-                }
+    namespaces = {'svg': 'http://www.w3.org/2000/svg',
+                  'xlink': "http://www.w3.org/1999/xlink"}
 
-imgSelectors = {"top_right_img": {'xpath': ".//*[@id='%s']"},
-                "mid_right_img": {'xpath': ".//*[@id='%s']"},
-                "main_img": {'xpath': ".//*[@id='%s']"},
-                "background_img": {'xpath': ".//*[@id='%s']"}
-                }
+    Textelectors = {"card_name": {'xpath': ".//*[@id='%s']/svg:tspan"},
+                    "card_subtitle": {'xpath': ".//*[@id='%s']/svg:tspan"},
+                    "category_": {'xpath': ".//*[@id='%s']/svg:tspan"},
+                    "val_": {'xpath': ".//*[@id='%s']/svg:tspan"},
+                    "description_1": {'xpath': ".//*[@id='%s']/svg:flowPara"},
+                    "description_2": {'xpath': ".//*[@id='%s']/svg:flowPara"},
+                    "top_right_text": {'xpath': ".//*[@id='%s']/svg:tspan"},
+                    "mid_right_text": {'xpath': ".//*[@id='%s']/svg:tspan"},
+                    }
 
-rectSelector = {"val_bar_": {'xpath': ".//*[@id='%s']"}
-                }
+    imgSelectors = {"top_right_img": {'xpath': ".//*[@id='%s']"},
+                    "mid_right_img": {'xpath': ".//*[@id='%s']"},
+                    "main_img": {'xpath': ".//*[@id='%s']"},
+                    "background_img": {'xpath': ".//*[@id='%s']"}
+                    }
 
-texts = {}
-imgs = {}
-rects = {}
+    rectSelector = {"val_bar_": {'xpath': ".//*[@id='%s']"}
+                    }
 
-for name, selector in Textelectors.items():
-    xpath = selector['xpath']
-    if name[-1] == '_':
-        for i in range(1, 5):
-            n = name + str(i)
-            texts[n] = {}
-            x = xpath % (n)
-            texts[n]['field'] = root.find(x, namespaces)
-    else:
-        x = xpath % (name)
-    texts[name] = {}
-    texts[name]['field'] = root.find(x, namespaces)
+    texts = {}
+    imgs = {}
+    rects = {}
 
-print("Printing text elements")
+    for name, selector in Textelectors.items():
+        xpath = selector['xpath']
+        if name[-1] == '_':
+            for i in range(1, 5):
+                n = name + str(i)
+                texts[n] = {}
+                x = xpath % (n)
+                texts[n]['field'] = root.find(x, namespaces)
+        else:
+            x = xpath % (name)
+        texts[name] = {}
+        texts[name]['field'] = root.find(x, namespaces)
 
-for name, field in texts.items():
-    field['field'].text += "huh"
-    print(name, field['field'], field['field'].text)
+    print("Printing text elements")
+
+    for name, field in texts.items():
+        if name in values:
+            field['field'].text = values[name]
+            print(name, field['field'], field['field'].text)
+
+    for name, selector in imgSelectors.items():
+        xpath = selector['xpath']
+        if name[-1] == '_':
+            for i in range(1, 5):
+                n = name + str(i)
+                imgs[n] = {}
+                x = xpath % (n)
+                imgs[n]['field'] = root.find(x, namespaces)
+        else:
+            x = xpath % (name)
+        imgs[name] = {}
+        imgs[name]['field'] = root.find(x, namespaces)
+
+    print("Printing imgs paths")
+
+    for name, field in imgs.items():
+        if name in values:
+            field['field'].set('{http://www.w3.org/1999/xlink}href','../../resourcen/'+values[name])
+            field['field'].set('{http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd}absref',os.getcwd()+'/../resourcen/'+values[name])
+        print(name, field['field'],
+              field['field'].get('{http://www.w3.org/1999/xlink}href'),
+              field['field'].get('{http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd}absref')
+              )
+
+    return tree
 
 
+def renderAsPdf(svgTree, name):
+    #  as new svg
+    tmp_file = name + '.svg'
+    pdf_file = name + '.pdf'
+    tmp_dir = '../out/tmp/'
+    pdf_dir = '../out/pdf/'
 
-for name, selector in imgSelectors.items():
-    xpath = selector['xpath']
-    if name[-1] == '_':
-        for i in range(1, 5):
-            n = name + str(i)
-            imgs[n] = {}
-            x = xpath % (n)
-            imgs[n]['field'] = root.find(x, namespaces)
-    else:
-        x = xpath % (name)
-    imgs[name] = {}
-    imgs[name]['field'] = root.find(x, namespaces)
+    for dir in [tmp_dir, pdf_dir]:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
 
-print("Printing imgs paths")
+    svgTree.write(tmp_dir + tmp_file)
 
-for name, field in imgs.items():
-    field['field'].set('{http://www.w3.org/1999/xlink}href','huhu.png')
-    field['field'].set('{http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd}absref',os.getcwd() + "huhu.png")
-    print(name, field['field'],
-          field['field'].get('{http://www.w3.org/1999/xlink}href'),
-          field['field'].get('{http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd}absref')
-          )
-
-# save as new xml
-tree.write('test_generated.svg')
+    # convert to pdf
+    x = Popen(['inkscape', tmp_dir + tmp_file, \
+               '--export-pdf=%s' % pdf_dir + pdf_file])
